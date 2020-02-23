4a562590ead6d1a668a385c91dc008aa7e9ad1e2
Don Chen <donchen@Dons-MacBook-Pro.local>
2020-02-22 16:55:48 -0800

    implement handleSubmit and handleValidateEmail

--- rsvp-form/client/src/components/App.jsx
+++ rsvp-form/client/src/components/App.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import axios from 'axios';
 import $ from 'jquery';
 
 
@@ -12,13 +13,40 @@ class App extends React.Component {
       guests: ''
     }
     this.handleChange = this.handleChange.bind(this);
-    // this.handleSubmit = this.handleChange.bind(this);
+    this.handleSubmit = this.handleChange.bind(this);
   }
 
   handleChange(e) {
     this.setState({
       [e.target.name]: e.target.value
     })
+  }
+
+  handleSubmit(e) {
+    const data = {
+      first: this.state.first,
+      last: this.state.last,
+      email: this.state.email,
+      guests: this.state.guests
+    };
+    if (this.handleValidateEmail(data.email)) {
+      axios.post('/rsvps', data)
+        .then(() => {
+          console.log('post request is successful');
+        })
+        .catch(() => {
+          console.log('post request is unsuccessful');
+        })
+    }
+}
+
+  handleValidateEmail(email) {
+    const chars = email.split('');
+    if (characters.include('@') && chars.indexOf('@') > 0 && chars.indexOf('@') < chars.length - 1) {
+      return true;
+    } else {
+      return false;
+    }
   }
 
   render() {
--- rsvp-form/server/index.js
+++ rsvp-form/server/index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const controller = require('../database/controllers/rsvp.js');
 
 const app = express();
 const PORT = 3000;
