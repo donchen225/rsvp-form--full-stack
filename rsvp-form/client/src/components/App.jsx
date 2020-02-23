68f666cb7b5064e6ce2e6e23cb6ff1412146a2c8
Don Chen <donchen@Dons-MacBook-Pro.local>
2020-02-22 16:36:03 -0800

    handle  change

--- rsvp-form/client/src/components/App.jsx
+++ rsvp-form/client/src/components/App.jsx
@@ -5,28 +5,34 @@ import $ from 'jquery';
 class App extends React.Component {
   constructor(props) {
     super(props);
-    // this.state = {
-    //   first = '',
-    //   last = '',
-    //   email = '',
-    //   guests = ''
-    // }
-    // this.handleChange = this.handleChange.bind(this);
+    this.state = {
+      first: '',
+      last: '',
+      email: '',
+      guests: ''
+    }
+    this.handleChange = this.handleChange.bind(this);
     // this.handleSubmit = this.handleChange.bind(this);
+  }
+
+  handleChange(e) {
+    this.setState({
+      [e.target.name]: e.target.value
+    })
   }
 
   render() {
     return (
     <form>
         <label>First Name</label>
-        <input type="text" id="first" name="first"></input>
+        <input type="text" id="first" name="first" onChange={(e) => {this.handleChange(e)}}></input>
         <label>Last Name</label>
-        <input type="text" id="last" name="last"></input>
+        <input type="text" id="last" name="last" onChange={(e) => {this.handleChange(e)}}></input>
         <label>Email Address</label>
-        <input type="email" id="email" name="email"></input>
+        <input type="email" id="email" name="email" onChange={(e) => {this.handleChange(e)}}></input>
         <label>Number of Guests</label>
-        <input type="text" id="guests" name="guests"></input>
-        <button type="button" onClick={(e) => this.handleSubmit(e)}>Submit</button>
+        <input type="text" id="guests" name="guests" onChange={(e) => {this.handleChange(e)}}></input>
+        <button type="button" onClick={(e) => {this.handleSubmit(e)}}>Submit</button>
     </form>
     )
   }
