cf619850ced23667a67290f31d7b19c239c05e08
Don Chen <donchen@Dons-MacBook-Pro.local>
2020-02-23 13:25:54 -0800

    trying to figure out why data is not being upserted into database

--- rsvp-form/database/controllers/rsvp.js
+++ rsvp-form/database/controllers/rsvp.js
@@ -4,7 +4,7 @@ const Rsvp = require('../models/rsvp.js');
 const findRsvpAndUpdate = (data, callback) => {
   const {first, last} = data;
   const filters = {first: first, last: last}; // how to pass filters so can check if filters do not already  exist, insert data. otherwise, if already exist, update it.
-  Rsvp.findOneAndUpdate(data, {new: true, upsert: true}, (err) => {
+  Rsvp.findOneAndUpdate(data, {upsert: true}, (err) => {
     if (err) {
       callback(err);
     } else {
@@ -12,5 +12,14 @@ const findRsvpAndUpdate = (data, callback) => {
     }
   });
 };
+
+// Rsvp.create({first: 'Donald', last: 'Chen', email: 'dochen225@berkeley.edu', guests: 1}, (err) => {
+//   if (err) {
+//     console.log('error in inserting first data');
+//   } else {
+//     console.log('insert is successful');
+//   }
+// })
+
 // request is successful but why is no data still in my db?
 module.exports = findRsvpAndUpdate;
